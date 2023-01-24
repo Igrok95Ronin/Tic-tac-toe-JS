7568ef28dc55c69e0b97f12b58813c8269afd94f
xl92@DESKTOP-F8KSG17 <xl92@mail.ru>
2023-01-24 21:51:58 +0300

    создали поле и ячейки

--- script/main.js
+++ script/main.js
@@ -1,6 +1,16 @@
 window.addEventListener('DOMContentLoaded', function () {
 	'use strict';/* JS сработает когда  DOM дерево подгрузится РЕКОМЕНДУЕТСЯ*/
 
+    const body = document.querySelector('body'),//создаем поле
+        wrapper = document.createElement('div');
+        body.appendChild(wrapper).classList.add('wrapper');
+
+        for(let i = 1; i <= 9; i++) {
+            const cells = document.createElement('div')
+            wrapper.appendChild(cells).classList.add('cells')
+            cells.textContent = 'X'
+        }
+        
 
 
 
@@ -23,5 +33,6 @@ window.addEventListener('DOMContentLoaded', function () {
 
 
 
-    
+
+
 })
