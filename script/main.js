eff6854b249dd726c07830e8c275e2027bc6d1ea
xl92@DESKTOP-F8KSG17 <xl92@mail.ru>
2023-01-25 20:57:03 +0300

    сделали границы ячеек из svg и X

--- script/main.js
+++ script/main.js
@@ -5,11 +5,30 @@ window.addEventListener('DOMContentLoaded', function () {
         wrapper = document.createElement('div');
         body.appendChild(wrapper).classList.add('wrapper');
 
-        for(let i = 1; i <= 9; i++) {
+        const svgLine = `<svg class="svgLine" height="216" width="216" >
+        <rect width='216' height='10' fill="#0da192" x="0" y="67" rx="5" ry="10"> </rect>
+        <rect width='216' height='10' fill="#0da192" x="0" y="139" rx="5" ry="10"> </rect>
+
+        <rect width='10' height='216' fill="#0da192" x="67" y="0" rx="5" ry="10"> </rect>
+        <rect width='10' height='216' fill="#0da192" x="139" y="0" rx="5" ry="10"> </rect>
+        </svg> `//границы ячеек из svg
+
+        const svgX = `<svg class="svgX" width="36" height="36">
+        <line x1="6" y1="6" x2="30" y2="30" stroke-linecap="round" ></line>
+        <line x1="6" y1="30" x2="30" y2="6" stroke-linecap="round" ></line>
+        </svg>`// рисуем Х из svg
+
+        wrapper.innerHTML = svgLine
+
+
+
+        for(let i = 1; i <= 9; i++) {//создаем ячейки
             const cells = document.createElement('div')
             wrapper.appendChild(cells).classList.add('cells')
-            cells.textContent = 'X'
+            cells.innerHTML = svgX
         }
+
+
         
 
 
