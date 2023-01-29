41d9ac2a2ad310b58a32ed5c029773179cbeb6e3
xl92@DESKTOP-F8KSG17 <xl92@mail.ru>
2023-01-29 17:20:40 +0300

    Реализована анимация Х и О

--- script/main.js
+++ script/main.js
@@ -8,6 +8,7 @@ window.addEventListener('DOMContentLoaded', function () {
         const svgLine = `<svg class="svgLine" height="216" width="216" >
         <rect width='216' height='10' fill="#0da192" x="0" y="67" rx="5" ry="10"> </rect>
         <rect width='216' height='10' fill="#0da192" x="0" y="139" rx="5" ry="10"> </rect>
+      
 
         <rect width='10' height='216' fill="#0da192" x="67" y="0" rx="5" ry="10"> </rect>
         <rect width='10' height='216' fill="#0da192" x="139" y="0" rx="5" ry="10"> </rect>
@@ -18,15 +19,44 @@ window.addEventListener('DOMContentLoaded', function () {
         <line x1="6" y1="30" x2="30" y2="6" stroke-linecap="round" ></line>
         </svg>`// рисуем Х из svg
 
+        const svgO = `<svg class="svgO">
+        <circle r="15" cx="25" cy="25"  fill="none" stroke-linecap="round"/>
+        </svg>`;// рисуем O из svg
+
         wrapper.innerHTML = svgLine
-
-
 
         for(let i = 1; i <= 9; i++) {//создаем ячейки
             const cells = document.createElement('div')
             wrapper.appendChild(cells).classList.add('cells')
-            cells.innerHTML = svgX
         }
+
+
+
+    const cells = document.querySelectorAll('.cells');//выбираем все ячейки
+        
+        //переменные
+        let player = true;
+
+        cells.forEach(elem => {
+            elem.addEventListener('click', main);
+        });
+
+        function main(e) {//Главная функция
+            players(e);
+        };
+
+        function players(e) {//выбора игрока X || O
+            if(player) {
+                e.target.innerHTML = svgX;
+                player = false;
+            } else {
+                e.target.innerHTML = svgO;
+                player = true;
+            }
+        }
+
+
+        
 
 
         
